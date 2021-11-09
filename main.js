3e4bcd67612e4bd4f5a8ce8d51958f110114f9fe
Dmi-tryBobrov <dmitrybobrovig@gmail.com>
2021-11-09 21:42:59 +0300

    added use of arrow keys

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import {createTile, moveUp, moveRight, moveDown, moveLeft, initializeField} from "./tiles.js";
 import {startNewGame, is2048} from "./utils.js";
 
+const keys = ["Space","ArrowUp","ArrowDown","ArrowLeft","ArrowRight"];
 const buttonNewGame = document.querySelector(".button-new-game");
 
 buttonNewGame.addEventListener("click", () => {
@@ -9,20 +10,27 @@ buttonNewGame.addEventListener("click", () => {
 });
 
 window.addEventListener("keydown", (e) =>{
+    if(keys.indexOf(e.code) > -1){
+        e.preventDefault();
+    }
     switch (e.code){
         case "KeyW":
+        case "ArrowUp":
             moveUp();     
             break;
             
         case "KeyD":
+        case "ArrowRight":
             moveRight();
             break;
 
         case "KeyA":
+        case "ArrowLeft":
             moveLeft();
             break;  
 
         case "KeyS":
+        case "ArrowDown":
             moveDown();
             break;
     }
